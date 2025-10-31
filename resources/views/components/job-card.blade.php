6a960bfd5109864216c8b805eaf979001b6d9c0a
Leonardo de Souza Oliveira <dev7@aproms.com.br>
2025-10-31 17:01:20 -0400

    first commit

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,19 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center ">
+    <div class="self-start text-sm">Laracast</div>
+
+    <div class="py-3 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+
+    <div class="flex justify-between item-center">
+        <div>
+
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+        {{--                <p>a</p>--}}
+        <img src="" alt="" class="rounded-xl">
+    </div>
+</div>
