cf7f79df93a6d26ea7f485ca713f7aa0654b181c
Leonardo <leosouza5@hotmail.com.br>
2025-11-01 15:21:34 -0300

    feat: finishing ui components

---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,9 @@
+@php
+    $classes =
+        'p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-800 group transition-colors duration-300';
+@endphp
+
+<div {{ $attributes->merge(['class' => $classes]) }}>
+
+    {{ $slot }}
+</div>
